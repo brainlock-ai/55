8204493c4f0613dbe66b966da5efb31112ede21c
kubernautis <edward@neuralinternet.ai>
2025-01-10 16:44:27 -0500

    check stake with rpc call

--- FHE/server/server.py
+++ FHE/server/server.py
@@ -8,22 +8,25 @@ Routes:
 All routes are protected with Epistula authentication.
 """
 
+import asyncio
 import io
+import json
 import os
 import uuid
 import time
+import base58
+import uvicorn
+import websockets
 from pathlib import Path
-from typing import Dict, Optional, Tuple
-from hashlib import sha256
+from typing import Dict, List, Optional, Tuple
+from hashlib import blake2b, sha256
 from time import perf_counter
 
-import uvicorn
 from fastapi import FastAPI, Form, HTTPException, UploadFile, Request, Response, Depends, File
 from fastapi.responses import FileResponse, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from loguru import logger
 from substrateinterface import Keypair
-import requests
 
 # No relative import here because when not used in the package itself
 from concrete.ml.deployment import FHEModelServer
@@ -38,10 +41,113 @@ class EpistulaVerifier:
     def __init__(self, allowed_delta_ms: int = 8000, cache_duration: int = 3600):
         self.ALLOWED_DELTA_MS = allowed_delta_ms
         self.MINER_HOTKEY = MINER_HOTKEY
-        self.stake_endpoint = "http://127.0.0.1:8091/verify_stake"
         self.cache_duration = cache_duration  # Cache duration in seconds
         self.stake_cache: Dict[str, Tuple[float, float, bool]] = {}  # hotkey -> (timestamp, stake, valid)
         logger.info(f"Initialized EpistulaVerifier with miner hotkey: {self.MINER_HOTKEY}")
+    
+    def ss58_decode(self, address: str, valid_ss58_format: Optional[int] = None) -> str:
+        """
+        Decodes given SS58 encoded address to an account ID
+        Parameters
+        ----------
+        address: e.g. EaG2CRhJWPb7qmdcJvy3LiWdh26Jreu9Dx6R1rXxPmYXoDk
+        valid_ss58_format
+
+        Returns
+        -------
+        Decoded string AccountId
+        """
+
+        # Check if address is already decoded
+        if address.startswith('0x'):
+            return address
+
+        if address == '':
+            raise ValueError("Empty address provided")
+
+        checksum_prefix = b'SS58PRE'
+
+        address_decoded = base58.b58decode(address)
+
+        if address_decoded[0] & 0b0100_0000:
+            ss58_format_length = 2
+            ss58_format = ((address_decoded[0] & 0b0011_1111) << 2) | (address_decoded[1] >> 6) | \
+                        ((address_decoded[1] & 0b0011_1111) << 8)
+        else:
+            ss58_format_length = 1
+            ss58_format = address_decoded[0]
+
+        if ss58_format in [46, 47]:
+            raise ValueError(f"{ss58_format} is a reserved SS58 format")
+
+        if valid_ss58_format is not None and ss58_format != valid_ss58_format:
+            raise ValueError("Invalid SS58 format")
+
+        # Determine checksum length according to length of address string
+        if len(address_decoded) in [3, 4, 6, 10]:
+            checksum_length = 1
+        elif len(address_decoded) in [5, 7, 11, 34 + ss58_format_length, 35 + ss58_format_length]:
+            checksum_length = 2
+        elif len(address_decoded) in [8, 12]:
+            checksum_length = 3
+        elif len(address_decoded) in [9, 13]:
+            checksum_length = 4
+        elif len(address_decoded) in [14]:
+            checksum_length = 5
+        elif len(address_decoded) in [15]:
+            checksum_length = 6
+        elif len(address_decoded) in [16]:
+            checksum_length = 7
+        elif len(address_decoded) in [17]:
+            checksum_length = 8
+        else:
+            raise ValueError("Invalid address length")
+
+        checksum = blake2b(checksum_prefix + address_decoded[0:-checksum_length]).digest()
+
+        if checksum[0:checksum_length] != address_decoded[-checksum_length:]:
+            raise ValueError("Invalid checksum")
+
+        return address_decoded[ss58_format_length:len(address_decoded)-checksum_length].hex()
+    
+    def convert_ss58_to_hex(self, ss58_address: str) -> str:
+        """Convert SS58 address to hex format."""
+        address_bytes = self.ss58_decode(ss58_address)
+        if isinstance(address_bytes, str):
+            address_bytes = bytes.fromhex(address_bytes)
+        return address_bytes.hex()
+        
+    async def call_rpc(self, call_params: List[str]) -> List[Dict]:
+        """Call the RPC and return the results."""
+        async with websockets.connect(self.chain_endpoint, ping_interval=None) as ws:
+            await ws.send(json.dumps({
+                "jsonrpc": "2.0",
+                "id": 1,
+                "method": "state_subscribeStorage",
+                'params': [call_params],
+            }))
+            await ws.recv()  # Ignore confirmation response
+            response = await ws.recv()
+            changes = json.loads(response)["params"]["result"]["changes"]
+            return changes
+    
+    def get_stake_from_hotkey(self, hotkey: str) -> float:
+        """Retrieve stakes for a list of hotkeys."""
+        stake = 0.0
+        call_params = [
+            "0x" 
+            + "658faa385070e074c85bf6b568cf0555"  # subtensorModule
+            + "7b4e834c482cd6f103e108dacad0ab65"  # totalHotkeyStake
+            + self.convert_ss58_to_hex(hotkey)
+        ]
+        call_results = asyncio.run(self.call_rpc(call_params))
+        
+        if call_results[1] is not None:
+            stake_hex = call_results[1][2:]
+            stake = self.hex_to_decimal(self.reverse_hex(stake_hex))
+            stake = round(stake / 1e9, 4)
+            print(hotkey, stake)
+        return stake
 
     def verify_stake(self, hotkey: str) -> tuple[bool, float, str]:
         current_time = time.time()
@@ -55,15 +161,8 @@ class EpistulaVerifier:
 
         # If not in cache or cache expired, verify with endpoint
         try:
-            response = requests.get(f"{self.stake_endpoint}/{hotkey}")
-            if response.status_code == 200:
-                data = response.json()
-                # Cache the result
-                self.stake_cache[hotkey] = (current_time, data["stake"], data["valid"])
-                if not data["valid"] and not data["no_force_permit"]:
-                    return False, data["stake"], f"Stake {data['stake']} below minimum requirement: 10000"
-                return data["valid"], data["stake"], data["error"]
-            return False, 0, f"Failed to verify stake: HTTP {response.status_code}"
+            stake = self.get_stake_from_hotkey(hotkey)
+            return True, stake, ""
         except Exception as e:
             logger.error(f"Stake verification error: {str(e)}")
             return False, 0, str(e)
